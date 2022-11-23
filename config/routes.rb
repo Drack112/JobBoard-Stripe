938357043fabc0ccbdd3b309629f11fc05fcd156
Drack112 <onepessoa3@gmail.com>
2022-11-23 10:40:59 -0300

    ✨ New Feature: Job Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resources :jobs
   devise_for :users
   root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
