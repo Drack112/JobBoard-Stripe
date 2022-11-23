41011869da95f8a523ecedcf2ede9690196d0337
Drack112 <onepessoa3@gmail.com>
2022-11-23 13:00:53 -0300

    ⚡ General Update:  Change the root routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   resources :jobs
   devise_for :users
-  root to: "home#index"
+  root to: "jobs#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
