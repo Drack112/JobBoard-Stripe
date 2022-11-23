42201ec9647b8d67164497046ac3d478e3b005dd
Drack112 <onepessoa3@gmail.com>
2022-11-23 18:13:23 -0300

    🐛 FIX: Stripe don't recognize api keys

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.configuration.stripe = {
+  publishable_key: ENV["stripe_publishable_key"],
+  secret_key: ENV["stripe_api_key"]
+}
+
+Stripe.api_key = ENV["stripe_api_key"]
